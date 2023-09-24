b92cc57eddbcc35893cb724293691f734fa073f5
widedboukouba <widedboukouba64@gmail.com>
2023-09-24 08:31:40 +0100

    responsive

--- product/product.js
+++ product/product.js
@@ -1,5 +1,26 @@
-let bigImg = document.querySelector('.big-img img');
-function showImg(pic){
-    bigImg.src = pic;
+let bigImg = document.querySelector(".big-img img");
+function showImg(pic) {
+  bigImg.src = pic;
 }
+
+const hamburger = document.querySelector(".hamburger");
+const tooltiptext = document.querySelector(".tooltiptext");
+const navbar = document.querySelector(".tooltip");
+
+hamburger.addEventListener("mouseenter", () => {
+  navbar.classList.add("opened");
+});
+
+hamburger.addEventListener("mouseleave", () => {
+  navbar.classList.remove("opened");
+});
+
+tooltiptext.addEventListener("mouseenter", () => {
+  navbar.classList.add("opened");
+});
+
+tooltiptext.addEventListener("mouseleave", () => {
+  navbar.classList.remove("opened");
+});
+
 
