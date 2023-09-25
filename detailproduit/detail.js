af93ae1810e1e84f0982109215f01817e15f0f1b
widedboukouba <widedboukouba64@gmail.com>
2023-09-25 08:46:40 +0100

    responsive

---
+++ detailproduit/detail.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelector(".hamburger");
+const tooltiptext = document.querySelector(".tooltiptext");
+const navbar = document.querySelector(".tooltip");
+
+hamburger.addEventListener("mouseenter", () => {
+  navbar.classList.add("opened");
+});
+
+hamburger.addEventListener("mouseleave", () => {
+  navbar.classList.remove("opened");
+});
+
+tooltiptext.addEventListener("mouseenter", () => {
+  navbar.classList.add("opened");
+});
+
+tooltiptext.addEventListener("mouseleave", () => {
+  navbar.classList.remove("opened");
+});
